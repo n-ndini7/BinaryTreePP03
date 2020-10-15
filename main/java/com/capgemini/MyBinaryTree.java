ae3e6516a8b79cd0a7fbe8de7c178bcd84101266
n-ndini7 <hp@RJs-PC>
2020-10-15 10:52:49 +0530

    [nandini] added master - welcome message

---
+++ main/java/com/capgemini/MyBinaryTree.java
@@ -0,0 +1,9 @@
+package com.capgemini;
+
+//Master branch - welcome message
+public class MyBinaryTree {
+
+	public static void main(String[] aregs) {
+		System.out.println("Welcome to Introduction to Binary Search Tree!");
+	}
+}
