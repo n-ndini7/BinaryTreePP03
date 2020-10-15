89e0f76ad90f34fc607d6432781ea76b7213b1c6
n-ndini7 <hp@RJs-PC>
2020-10-15 10:59:39 +0530

    [nandini] added UC1 - create binary search tree

--- main/java/com/capgemini/MyBinaryTree.java
+++ main/java/com/capgemini/MyBinaryTree.java
@@ -1,9 +1,25 @@
 package com.capgemini;
 
-//Master branch - welcome message
-public class MyBinaryTree {
+//UC1 - create Binary Search Tree
+public class MyBinaryTree<K extends Comparable<K>> {
 
-	public static void main(String[] aregs) {
-		System.out.println("Welcome to Introduction to Binary Search Tree!");
+	private MyBinaryNode<K> root;
+
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
 	}
+
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+		if (current == null)
+			return new MyBinaryNode<>(key);
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0)
+			return current;
+		if (compareResult < 0)
+			current.left = addRecursively(current.left, key);
+		else
+			current.right = addRecursively(current.right, key);
+		return current;
+	}
+
 }
